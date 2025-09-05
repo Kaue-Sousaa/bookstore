6d94c8c0ca011da1bd561aa1cba654a026eec5df
Kaue Sousa <kauelimasousa02@gmail.com>
2025-09-04 21:22:44 -0300

    feat: initial commit microservice monitoring

---
+++ monitoring/src/main/java/com/bookstore/system/monitoring/enums/AlertType.java
@@ -0,0 +1,11 @@
+package com.bookstore.system.monitoring.enums;
+
+public enum AlertType {
+
+    API_DOWN,
+    HIGH_LATENCY,
+    ERROR_RATE_HIGH,
+    SLA_BREACH,
+    CONSECUTIVE_FAILS,
+    CERTIFICATE_EXPIRY
+}
---
+++ monitoring/src/main/java/com/bookstore/system/monitoring/enums/HealthCheckType.java
@@ -0,0 +1,11 @@
+package com.bookstore.system.monitoring.enums;
+
+public enum HealthCheckType {
+
+    HTTP_STATUS,
+    RESPONSE_TIME,
+    CONTENT_MATCH,
+    JSON_SCTRUCTURE,
+    DATABASE_QUERY,
+    CUSTOM
+}
